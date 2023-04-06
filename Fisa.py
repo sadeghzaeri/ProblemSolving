758a0f0b826822885f6bd5323b38e858107fec9a
sadegh <m.sadegh.zaeri@gmail.com>
2023-04-06 19:22:56 +0430

    4th

---
+++ Fisa.py
@@ -0,0 +1,16 @@
+#quera.org/problemset/280/
+a = int(input())
+b = int(input())
+c = int(input())
+
+if ((a**2) == (b**2) + (c**2)):
+    print("YES")
+
+elif ((c**2) == (b**2) + (a**2)):
+    print("YES")
+
+elif ((b**2) == (c**2) + (a**2)):
+    print("YES")
+
+else:
+    print("NO")
